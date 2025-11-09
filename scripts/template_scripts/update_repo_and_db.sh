5c2be5826aae321a7728105c510250d0e7820448
Borovlev A. S <bas@qdes.com.ua>
2025-11-09 23:12:35 +0000

    Remove git pull for main repository and add pwd debug output in update script

--- scripts/template_scripts/update_repo_and_db.sh
+++ scripts/template_scripts/update_repo_and_db.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-git reset --hard
-git pull
 git submodule foreach --recursive git reset --hard
 git submodule foreach --recursive 'git fetch origin --tags && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo "main") && git pull'
 
+pwd
 bash ./scripts_local/update_database.sh
