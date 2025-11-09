19f657dd586b326971d04c81c6a5660d94d96bfc
Borovlev A. S <bas@qdes.com.ua>
2025-11-09 23:15:07 +0000

    [FIX] Script filename fixed

--- scripts/template_scripts/update_repo_and_db.sh
+++ scripts/template_scripts/update_repo_and_db.sh
@@ -3,5 +3,4 @@
 git submodule foreach --recursive git reset --hard
 git submodule foreach --recursive 'git fetch origin --tags && git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo "main") && git pull'
 
-pwd
-bash ./scripts_local/update_database.sh
+bash ./scripts_local/update_db.sh
